b1e1a8a33435819241e73c11964189175c060fe7
Tejas jaiswal <tejasjaiswal852@gmail.comm>
2023-07-25 22:27:50 +0530

    1st update

--- main.go
+++ main.go
@@ -17,5 +17,27 @@ func main() {
 	//
 
 	var conferenv = "conferenece"
+	const conferrenceTickets = 50
+	var remaningTickets = 50
+
 	fmt.Println("hello heloo", conferenv, ",bello beloo")
+	fmt.Println("we have total of this many tickets", remaningTickets)
+	fmt.Println("welcome to", conferenv, "my dear friends")
+	fmt.Printf("welcome to %v my dear friends\n", conferenv)
+
+	// %v is a placeholder
+
+	var userName string
+
+	// you need to tell the type of the variable
+	var userTicket int
+	userName = "sai"
+	userTicket = 10
+
+	// here the 'Printf' is used to print the string and the variable
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTicket)
+
 }
+
+// if you initilize the var or import the module and dont use it --you'll get an error
