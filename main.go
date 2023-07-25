4a8700dfc425af062d5971451c8dd1f8c6ff5370
Tejas jaiswal <tejasjaiswal852@gmail.comm>
2023-07-25 21:33:39 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// fmt meaning --format package
+// it's a compiled language and starts executing from main function.
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("yess we are learning go")
+	fmt.Println("Hello, World!")
+	fmt.Println("yess we are learning go")
+
+	// println and Println is differnent.
+	//
+
+	var conferenv = "conferenece"
+	fmt.Println("hello heloo", conferenv, ",bello beloo")
+}
